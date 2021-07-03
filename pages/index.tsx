62e345fe4ff9fb6670d0a671d9a4b32fee104c2c
Iwan <iwan.maksackov@yandex.ru>
2021-07-03 16:38:22 +0300

    add base layout and route to sidebar menu

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import React, {useState} from 'react';
 import {Button, Htag, Ptag, Rating, Tag} from '../components';
 import {withLayout} from "../layout/Layout";
+import {GetStaticProps} from "next";
+import axios from "axios";
+import {MenuItem} from "../interfaces/menu.interface";
 
-function Home(): JSX.Element {
+function Home({menu}: HomeProps): JSX.Element {
     const [rat, setRat] = useState<number>(2)
     return (
     <>
@@ -15,8 +18,30 @@ function Home(): JSX.Element {
         <Tag size='m' color='green'>Medium</Tag>
         <Tag color='primary'>Small</Tag>
         <Rating rating={rat} isEditable={true} setRating={setRat}/>
+        <ul>
+            {menu.map(m => (<li key={m._id.secondCategory}>{m._id.secondCategory}</li>))}
+        </ul>
+
     </>
     );
 }
 
 export default withLayout(Home)
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+    const firstCategory = 0
+    const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+        firstCategory
+    })
+    return {
+        props: {
+            menu,
+            firstCategory
+        }
+    }
+}
+
+interface HomeProps extends Record<string, unknown>{
+    menu: MenuItem[],
+    firstCategory: number
+}
